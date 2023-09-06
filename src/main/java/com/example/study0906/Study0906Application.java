53015a4a9ef5c6b888b15d1efd466566bc6c4950
장관영 <jkypch@adcapsule.co.kr>
2023-09-06 16:58:27 +0900

    first commit

---
+++ src/main/java/com/example/study0906/Study0906Application.java
@@ -0,0 +1,13 @@
+package com.example.study0906;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study0906Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study0906Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/study0906/Study0906ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study0906;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study0906ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
